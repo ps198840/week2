fd1733d34dfa09fdb291453271c6b864db8ecdfe
arasg <ps198840@summacollege.nl>
2022-09-16 15:01:03 +0200

    week2-opdracht1

---
+++ resources/views/student.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+</head>
+<body>
+<h1>Test met controller</h1>
+<h2>Je naam is: {{ $naam }}</h2>
+</body>
+</html>
+
+
